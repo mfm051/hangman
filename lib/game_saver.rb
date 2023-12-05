48998891852aec8f52a7c5f2264983b2a79e6d6d
mfm051 <mateusmatos051@gmail.com>
2023-12-05 14:11:25 -0300

    Add #standard_filename to GameSaver

--- lib/game_saver.rb
+++ lib/game_saver.rb
@@ -2,22 +2,19 @@
 
 # Saves and loads games
 module GameSaver
-  def save(game)
+  def save(game, filename)
     make_dir
 
     File.open(filename, 'w') { Marshal.dump(game) }
-  end
-
-  def filename
-    puts 'Choose a name to file'
-    chosen_filename = gets.chomp
-
-    return "saved/#{chosen_filename}" unless chosen_filename.empty?
-
-    "saved/#{Time.now.to_i}"
+  rescue StandardError
+    File.open(standard_filename, 'w') { Marshal.dump(game) }
   end
 
   def make_dir
     Dir.mkdir('saved') unless Dir.exist?('saved')
+  end
+
+  def standard_filename
+    "saved/#{Time.now.to_i}"
   end
 end
